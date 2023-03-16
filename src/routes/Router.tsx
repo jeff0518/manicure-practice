a2594b7ce599396382fdfbf66f41c16fa3cdbe09
jeff0518 <cycivs07@icloud.com>
2023-03-17 00:04:03 +0800

    feat: add toggleClass

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -4,13 +4,20 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Home from "../pages/Home";
 import AdminLogin from "../pages/AdminLogin";
+import RootLayout from "./RootLayout";
 
 const Router = () => {
   const router = createBrowserRouter([
-    { path: "/login", element: <Login /> },
-    { path: "/register", element: <Register /> },
-    { path: "/admin", element: <AdminLogin />, children: [] },
-    { path: "/", element: <Home />, children: [] },
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { path: "/login", element: <Login /> },
+        { path: "/register", element: <Register /> },
+        { path: "/admin", element: <AdminLogin />, children: [] },
+        { path: "/home", element: <Home />, children: [] },
+      ],
+    },
   ]);
   return (
     <>
@@ -19,4 +26,4 @@ const Router = () => {
   );
 };
 
-export default Router
+export default Router;
