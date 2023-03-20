a876cb6d84e7a020ea8f40a818bcd3c27b712750
jeff0518 <cycivs07@icloud.com>
2023-03-21 04:51:15 +0800

    fix: bug

--- src/components/Auth/RegisterForm.tsx
+++ src/components/Auth/RegisterForm.tsx
@@ -1,61 +1,27 @@
-import { useRef, useContext, useState, useReducer } from "react";
+import { useRef, useContext, useState, useReducer, useEffect } from "react";
 import { Link } from "react-router-dom";
 import InputUI from "../UI/inputs/InputUI";
 import ButtonUI from "../UI/buttons/ButtonUI";
-import { CurrentContext } from "../contexts/usersContext/CurrentUserContext";
+// import { CurrentContext } from "../contexts/usersContext/CurrentUserContext";
 import style from "./RegisterForm.module.scss";
 
+type RegisterPageProps = {
+  inputValues: {  userName: string;
+  account: string;
+  password: string;
+  checkPassword: string;};
+  errorMessage: {};
+  formIsValid: React.MutableRefObject<string>;
+  inputChangeHandler: (action: { type: string; payload: string }) => void;
+  submitHandler: (event: React.FormEvent) => void;
+}
 
-  const initialInput = {
-    userName: "",
-    account: "",
-    password: "",
-    checkPassword: "",
-  };
-
-  const actions = {
-    userName: "userName",
-    account: "account",
-    password: "password",
-    checkPassword: "checkPassword",
-  };
-
-  const inputReducer = (state: any, action: { type: any; payload: any; }) => {
-    switch (action.type) {
-      case actions.userName:
-        return { ...state, userName: action.payload };
-      case actions.account:
-        return { ...state, account: action.payload };
-      case actions.password:
-        return { ...state, password: action.payload };
-      case actions.checkPassword:
-        return { ...state, checkPassword: action.payload };
-      default:
-        return state;
-    }
-  };
-
-const RegisterForm = () => {
-  const userDataCtx = useContext(CurrentContext);
-  const [inputValues, dispatch] = useReducer(inputReducer, initialInput);
-  const [errMsg, setErrMsg] = useState({});
-
+const RegisterForm = (props: RegisterPageProps) => {
+  const errorMessage = props.errorMessage;
+  console.log(errorMessage);
   
-  
-
-  const inputChangeHandler = (action: { type: any; payload: any; }) => {
-    const {type, payload} = action
-    dispatch(action)
-    console.log(payload);
-    console.log(type);
-    
-    
-  }
-  const submitHandler = (event: React.FormEvent) => {
-    event.preventDefault();
-  };
   return (
-    <form className={style.form} onSubmit={submitHandler}>
+    <form className={style.form} onSubmit={props.submitHandler}>
       <h1 className={style.form__title}>建立您的帳號</h1>
       <div className={style.input__container}>
         <InputUI
@@ -63,9 +29,12 @@ const RegisterForm = () => {
           inputName="姓名"
           inputPlaceHolder="怎麼稱呼您"
           inputType="text"
-          inputValue={inputValues.userName}
-          onChange={inputChangeHandler}
+          inputValue={props.inputValues.userName}
+          onChange={props.inputChangeHandler}
         />
+        {props.formIsValid && (
+          <p className={style.error}>{errorMessage.userName}</p>
+        )}
       </div>
       <div className={style.input__container}>
         <InputUI
@@ -73,9 +42,12 @@ const RegisterForm = () => {
           inputName="帳號"
           inputPlaceHolder="請輸入您的帳號"
           inputType="text"
-          inputValue={inputValues.account}
-          onChange={inputChangeHandler}
+          inputValue={props.inputValues.account}
+          onChange={props.inputChangeHandler}
         />
+        {props.formIsValid && (
+          <p className={style.error}>{errorMessage.account}</p>
+        )}
       </div>
       <div className={style.input__container}>
         <InputUI
@@ -83,9 +55,12 @@ const RegisterForm = () => {
           inputName="密碼"
           inputPlaceHolder="請輸入密碼"
           inputType="password"
-          inputValue={inputValues.password}
-          onChange={inputChangeHandler}
+          inputValue={props.inputValues.password}
+          onChange={props.inputChangeHandler}
         />
+        {props.formIsValid && (
+          <p className={style.error}>{errorMessage.password}</p>
+        )}
       </div>
       <div className={style.input__container}>
         <InputUI
@@ -93,12 +68,19 @@ const RegisterForm = () => {
           inputName="確認密碼"
           inputPlaceHolder="請再次輸入密碼"
           inputType="password"
-          inputValue={inputValues.checkPassword}
-          onChange={inputChangeHandler}
+          inputValue={props.inputValues.checkPassword}
+          onChange={props.inputChangeHandler}
         />
+        {props.formIsValid && (
+          <p className={style.error}>{errorMessage.checkPassword}</p>
+        )}
       </div>
 
-      <ButtonUI btnStyle="btn__pill__large" text="提交" />
+      <ButtonUI
+        btnStyle="btn__pill__large"
+        text="提交"
+        disabled={!props.formIsValid ? true : false}
+      />
       <div className={style.form__actions}>
         <Link to="/auth">
           <ButtonUI btnStyle="link" text="登入" />
