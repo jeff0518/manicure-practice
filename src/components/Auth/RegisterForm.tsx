f05928abc08b0f615576fe8f394588eea9b1ab16
jeff0518 <cycivs07@icloud.com>
2023-03-21 02:33:33 +0800

    feat: event register

--- src/components/Auth/RegisterForm.tsx
+++ src/components/Auth/RegisterForm.tsx
@@ -1,12 +1,61 @@
+import { useRef, useContext, useState, useReducer } from "react";
 import { Link } from "react-router-dom";
 import InputUI from "../UI/inputs/InputUI";
 import ButtonUI from "../UI/buttons/ButtonUI";
-
+import { CurrentContext } from "../contexts/usersContext/CurrentUserContext";
 import style from "./RegisterForm.module.scss";
 
+
+  const initialInput = {
+    userName: "",
+    account: "",
+    password: "",
+    checkPassword: "",
+  };
+
+  const actions = {
+    userName: "userName",
+    account: "account",
+    password: "password",
+    checkPassword: "checkPassword",
+  };
+
+  const inputReducer = (state: any, action: { type: any; payload: any; }) => {
+    switch (action.type) {
+      case actions.userName:
+        return { ...state, userName: action.payload };
+      case actions.account:
+        return { ...state, account: action.payload };
+      case actions.password:
+        return { ...state, password: action.payload };
+      case actions.checkPassword:
+        return { ...state, checkPassword: action.payload };
+      default:
+        return state;
+    }
+  };
+
 const RegisterForm = () => {
+  const userDataCtx = useContext(CurrentContext);
+  const [inputValues, dispatch] = useReducer(inputReducer, initialInput);
+  const [errMsg, setErrMsg] = useState({});
+
+  
+  
+
+  const inputChangeHandler = (action: { type: any; payload: any; }) => {
+    const {type, payload} = action
+    dispatch(action)
+    console.log(payload);
+    console.log(type);
+    
+    
+  }
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
   return (
-    <form className={style.form}>
+    <form className={style.form} onSubmit={submitHandler}>
       <h1 className={style.form__title}>建立您的帳號</h1>
       <div className={style.input__container}>
         <InputUI
@@ -14,7 +63,8 @@ const RegisterForm = () => {
           inputName="姓名"
           inputPlaceHolder="怎麼稱呼您"
           inputType="text"
-          inputValue=""
+          inputValue={inputValues.userName}
+          onChange={inputChangeHandler}
         />
       </div>
       <div className={style.input__container}>
@@ -23,7 +73,8 @@ const RegisterForm = () => {
           inputName="帳號"
           inputPlaceHolder="請輸入您的帳號"
           inputType="text"
-          inputValue=""
+          inputValue={inputValues.account}
+          onChange={inputChangeHandler}
         />
       </div>
       <div className={style.input__container}>
@@ -32,7 +83,8 @@ const RegisterForm = () => {
           inputName="密碼"
           inputPlaceHolder="請輸入密碼"
           inputType="password"
-          inputValue=""
+          inputValue={inputValues.password}
+          onChange={inputChangeHandler}
         />
       </div>
       <div className={style.input__container}>
@@ -41,7 +93,8 @@ const RegisterForm = () => {
           inputName="確認密碼"
           inputPlaceHolder="請再次輸入密碼"
           inputType="password"
-          inputValue=""
+          inputValue={inputValues.checkPassword}
+          onChange={inputChangeHandler}
         />
       </div>
 
---
+++ src/components/User/AdminUserCard.tsx
@@ -0,0 +1,12 @@
+import InputUI from "../UI/inputs/InputUI";
+import SelectUI from "../UI/selects/SelectUI";
+import ButtonUI from "../UI/buttons/ButtonUI";
+
+import style from './AdminUserCard.module.scss'
+const AdminUserCard = (props) => {
+  return (
+    
+  );
+}
+
+export default AdminUserCard;
